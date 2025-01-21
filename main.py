67191a2bb5d5bc74b57261b1a3c7a52fd30836f3
jkr003 <jkrollins003@gmail.com>
2025-01-21 15:45:35 -0700

    Add groups for objects in the game and use those for updateing and drawing those objects

--- main.py
+++ main.py
@@ -6,19 +6,28 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2,  SCREEN_HEIGHT / 2)
+    
     dt = 0
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        for obj in updateable:
+            obj.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        for obj in drawable:           
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
